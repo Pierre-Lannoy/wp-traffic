e894120f0e3ee4c80613b712196749e14c20369a
Pierre Lannoy <pierre.lannoy@adeo.com>
2019-09-11 19:09:01 +0200

    Fix "Trying to get property 'blogname' of non-object" when the blog has been deleted.

--- includes/system/class-blog.php
+++ includes/system/class-blog.php
@@ -41,7 +41,10 @@ class Blog {
 	public static function get_blog_name( $id = null, $default = 'unknown' ) {
 		if ( $id && is_numeric( $id ) && $id > 0 && Environment::is_wordpress_multisite() ) {
 			$blog_info = get_blog_details( $id );
-			return $blog_info->blogname;
+			if ( is_object( $blog_info ) ) {
+				return $blog_info->blogname;
+			}
+			return '- deleted site -';
 
 		} elseif ( $id && is_numeric( $id ) && $id > 0 ) {
 			return get_bloginfo( 'name' );
@@ -49,6 +52,23 @@ class Blog {
 		} else {
 			return $default;
 		}
+	}
+
+	/**
+	 * Get a fully qualified blog name.
+	 *
+	 * @param   mixed $id         Optional. The blog id.
+	 * @return  string  The blog name if detected, $default otherwise.
+	 * @since   1.3.0
+	 */
+	public static function get_full_blog_name( $id = 0 ) {
+		if ( is_numeric( $id ) ) {
+			return sprintf( '"%s" (blog ID %s)', self::get_blog_name( $id ), $id );
+		}
+		if ( 'WP_Site' === get_class( $id ) ) {
+			return sprintf( '"%s" (blog ID %s)', (string) $id->blogname, $id->id );
+		}
+		return 'unknow blog';
 	}
 
 	/**
