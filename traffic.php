69f8fdf25c709c1cb181fc1c897402cf882388cb
Pierre Lannoy <pierre.lannoy@adeo.com>
2019-11-03 17:36:08 +0100

    Ready to release 1.2.2 on wp.org.

--- traffic.php
+++ traffic.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Traffic
  * Plugin URI:        https://github.com/Pierre-Lannoy/wp-traffic
  * Description:       Full featured analytics for WordPress APIs.
- * Version:           1.2.1
+ * Version:           1.2.2
  * Author:            Pierre Lannoy
  * Author URI:        https://pierre.lannoy.fr
  * License:           GPLv2 or later
