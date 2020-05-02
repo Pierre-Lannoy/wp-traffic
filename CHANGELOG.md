83830439d54409be76af22b0a6d119af75cc62e7
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-05-02 13:06:22 +0200

    Fixes issues with IIS/Win10.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ All notable changes to **Traffic** are documented in this *changelog*.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and **Traffic** adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ##[Unreleased - Will be 1.6.2]
+### Fixed
+- There's an error while activating the plugin when the server is Microsoft IIS with Windows 10.
 
 ## [1.6.1] - 2020-04-10
 ### Fixed
--- includes/system/class-environment.php
+++ includes/system/class-environment.php
@@ -35,6 +35,7 @@ class Environment {
 	 */
 	public static function init() {
 		$plugin_path         = str_replace( TRAFFIC_SLUG . '/includes/system/', TRAFFIC_SLUG . '/', plugin_dir_path( __FILE__ ) );
+		$plugin_path         = str_replace( TRAFFIC_SLUG . '\includes\system/', TRAFFIC_SLUG . '/', $plugin_path );
 		$plugin_url          = str_replace( TRAFFIC_SLUG . '/includes/system/', TRAFFIC_SLUG . '/', plugin_dir_url( __FILE__ ) );
 		$plugin_relative_url = str_replace( get_site_url() . '/', '', $plugin_url );
 		define( 'TRAFFIC_PLUGIN_DIR', $plugin_path );
