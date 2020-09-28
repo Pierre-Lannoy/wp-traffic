501ad427e8ff06eee08df136e2675d1c3f32a09a
Pierre Lannoy <pierre@lannoy.fr>
2020-09-28 09:29:42 +0200

    Fixes wrong path.

--- includes/system/class-markdown.php
+++ includes/system/class-markdown.php
@@ -45,7 +45,7 @@ class Markdown {
 		$mode        = $_attributes['mode'];
 		$error       = esc_html__( 'Sorry, unable to find or read the specified file.', 'traffic' );
 		$result      = esc_html( $error );
-		$changelog   = DECALOG_PLUGIN_DIR . $file;
+		$changelog   = TRAFFIC_PLUGIN_DIR . $file;
 		if ( file_exists( $changelog ) ) {
 			try {
 				// phpcs:ignore
