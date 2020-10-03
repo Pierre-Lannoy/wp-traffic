2df86c7b06a031f4b58cc401a12d45c70a381468
Pierre Lannoy <pierre@lannoy.fr>
2020-10-03 15:50:10 +0200

    Adds PHP 7.3 compatibility for `array_key_last()` and `array_key_first`.

--- functions.php
+++ functions.php
@@ -47,3 +47,27 @@ function traffic_mb_str_pad( $input, $length, $padding = ' ', $padType = STR_PAD
 	}
 	return $result;
 }
+
+/**
+ * Provide PHP 7.3 compatibility for array_key_last() function.
+ */
+if ( ! function_exists( 'array_key_last' ) ) {
+	// phpcs:ignore
+	function array_key_last( array $array ) {
+		if ( ! empty( $array ) ) {
+			return key( array_slice( $array, -1, 1, true ) );
+		}
+	}
+}
+
+/**
+ * Provide PHP 7.3 compatibility for array_key_first() function.
+ */
+if ( ! function_exists( 'array_key_first' ) ) {
+	// phpcs:ignore
+	function array_key_first( array $arr ) {
+		foreach ( $arr as $key => $unused ) {
+			return $key;
+		}
+	}
+}
