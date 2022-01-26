3dbaf32a76e62d5225310255f85438920f57faa3
Pierre Lannoy <pierre@lannoy.fr>
2022-01-26 17:49:10 +0100

    Site Health page now presents a much more realistic test about object caching.

--- includes/system/class-sitehealth.php
+++ includes/system/class-sitehealth.php
@@ -178,32 +178,21 @@ class Sitehealth {
 	public static function perfopsone_test_objectcache_do() {
 		$key       = 'perfopsone_objectcache';
 		$analytics = Cache::get_analytics();
-		if ( 'db_transient' === $analytics['type'] ) {
-			$result = [
-				'label'       => esc_html__( 'You should use object caching', 'traffic' ),
-				'status'      => 'recommended',
-				'badge'       => [
-					'label' => esc_html__( 'Performance', 'traffic' ),
-					'color' => 'orange',
-				],
-				'description' => sprintf( '<p>%s %s</p>', esc_html__( 'Your site uses database transient.', 'traffic' ), esc_html__( 'You should consider using a dedicated object caching mechanism, like Memcached or Redis, to improve your site\'s speed.', 'traffic' ) ),
-				'actions'     => '',
-				'test'        => $key,
-			];
-		} elseif ( 'apcu' === $analytics['type'] ) {
-			$result = [
+		if ( 'apcu' === $analytics['type'] ) {
+			return [
 				'label'       => esc_html__( 'You should improve object caching', 'traffic' ),
 				'status'      => 'recommended',
 				'badge'       => [
 					'label' => esc_html__( 'Performance', 'traffic' ),
 					'color' => 'blue',
 				],
-				'description' => sprintf( '<p>%s %s</p>', esc_html__( 'Your site uses APCu, but only a few plugins know how to take advantage of it.', 'traffic' ), esc_html__( 'You should consider using a dedicated object caching mechanism, like Memcached or Redis, to improve your site\'s speed.', 'traffic' ) ),
+				'description' => sprintf( '<p>%s %s</p>', esc_html__( 'APCu is available on your site, but only PerfOps One suite and some few other plugins know how to take advantage of it.', 'traffic' ), sprintf( esc_html__( 'You should consider using %s to improve your site\'s speed.', 'traffic' ), '<a href="https://perfops.one/apcu-manager/">APCu Manager</a>' ) ),
 				'actions'     => '',
 				'test'        => $key,
 			];
-		} elseif ( 'object_cache' === $analytics['type'] ) {
-			$result = [
+		}
+		if ( 'object_cache' === $analytics['type'] ) {
+			return [
 				'label'       => esc_html__( 'Your site uses object caching', 'traffic' ),
 				'status'      => 'good',
 				'badge'       => [
@@ -215,7 +204,17 @@ class Sitehealth {
 				'test'        => $key,
 			];
 		}
-		return $result;
+		return [
+			'label'       => esc_html__( 'You should use object caching', 'traffic' ),
+			'status'      => 'recommended',
+			'badge'       => [
+				'label' => esc_html__( 'Performance', 'traffic' ),
+				'color' => 'orange',
+			],
+			'description' => sprintf( '<p>%s %s</p>', esc_html__( 'Your site uses database transient.', 'traffic' ), esc_html__( 'You should consider using a dedicated object caching mechanism, like APCu, Memcached or Redis, to improve your site\'s speed.', 'traffic' ) ),
+			'actions'     => '',
+			'test'        => $key,
+		];
 	}
 
 	/**
