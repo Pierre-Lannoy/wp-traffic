7c238feb89c45ead049dd206adadbc6efaaf1b6a
Pierre Lannoy <pierre@hosterra.eu>
2024-05-07 16:13:43 +0200

    Ready to release 2.14.0 on wp.org

--- traffic.php
+++ traffic.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Traffic
  * Plugin URI:        https://perfops.one/traffic
  * Description:       Full featured monitoring & analytics for WordPress APIs.
- * Version:           2.13.2
+ * Version:           2.14.0
  * Requires at least: 6.2
  * Requires PHP:      8.1
  * Author:            Pierre Lannoy / PerfOps One
